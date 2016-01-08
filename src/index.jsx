c2fa78bf4a85547f060f7ae5bfa2171355a8ad6b
Brandon Horst <brandonhorst@gmail.com>
2016-01-08 14:43:11 -0500

    Updated for new suppressWhen functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,57 @@
 /** @jsx createElement */
-import _ from 'lodash'
 import {createElement, Phrase} from 'lacona-phrase'
 
-export default class URL {
-  getValue (result) {
-    if (!result) return
-
-    return `${result.protocol}${result.remaining}`
-  }
-  // validate (input) {
-  //   return /^(?:(?:https?):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i.test(input)
-  // }
-  //
-  // displayWhen (input) {
-  //   return /^(https?:\/\/|h$|ht$|htt$|https?$|https?:$|https?:\/$|https?:\/\/$)?([\w\d-]+(\.\w{0,18}\S*)?)?$/i.test(input)
-  // }
-  validateWithProtocol (input) {
-    return /^(https?\:\/\/)([-_a-z0-9]+\.)+[-a-z]+\S+$/i.test(input)
+export class URL extends Phrase {
+  static defaultProps = {
+    splitOn: /\s|,/,
+    argument: 'URL',
+    limit: 1
   }
 
-  displayWhen (input) {
-    return /^(https?:\/\/|h$|ht$|htt$|http$|https$|http:$|http:\/$|https:$|https:\/$)?[-_a-z0-9]*(\.\S*)?$/.test(input)
-    // return /^(\w{3,8}:\/\/|\w{3,8}$|\w{3,8}:$|\w{3,8}:\/$)?([\w\d-]+(\.\w{0,18}\S*)?)?$/i.test(input)
-  }
-
-  validateWithoutProtocol (input) {
-    return /^([-_a-z0-9]+\.)+[a-z]+\S+$/i.test(input)
+  suppressWhen (input) {
+    return /^(h|ht|htt|https?|https?:|https?:\/|https?:\/\/|(https?:\/\/)?|\w*|\w+\.|\w+\.[a-z])$/.test(input)
   }
 
   describe() {
     return (
-      <argument text='URL' displayWhen={this.displayWhen}>
-        <sequence>
-          <choice limit={1} id='protocol'>
-            <decorator text='http://' value='http://' />
-            <decorator text='https://' value='https://' />
-          </choice>
-          <freetext validate={this.validateWithoutProtocol} splitOn={this.props.splitOn} limit={this.props.limit} id='remaining' />
-        </sequence>
-      </argument>
+      <label text={this.props.argument} suppressWhen={this.suppressWhen}>
+        <choice>
+          <ImpliedURL {...this.props} />
+          <SpecifiedURL {...this.props} />
+        </choice>
+      </label>
     )
   }
 }
 
-URL.defaultProps = {
-  splitOn: ' ',
-  limit: 1
+class ImpliedURL extends Phrase {
+  getValue (result) {
+    return `http://${result.remaining}`
+  }
+
+  filter (input) {
+    return /^(\w+\.)+\w{2,63}\S*$/i.test(input)
+  }
+
+  describe () {
+    return (
+      <map function={this.getValue}>
+        <sequence>
+          <literal decorate allowInput={false} text='http://'/>
+          <freetext filter={this.filter} splitOn={this.props.splitOn} limit={this.props.limit} id='remaining' />
+        </sequence>
+      </map>
+    )
+  }
+
+}
+
+class SpecifiedURL extends Phrase {
+  filter (input) {
+    return /^https?:\/\/\S+/i.test(input)
+  }
+
+  describe () {
+    return <freetext filter={this.filter} splitOn={this.props.splitOn} limit={this.props.limit} />
+  }
 }
--- test/test.jsx
+++ test/test.jsx
@@ -1,30 +1,91 @@
 /** @jsx createElement */
 /* eslint-env mocha */
 
-import {createElement, Phrase} from 'lacona-phrase'
-import URL from '..'
-import {expect} from 'chai'
-import fulltext from 'lacona-util-fulltext'
-import {Parser} from 'lacona'
+import _ from 'lodash'
+import { createElement, Phrase } from 'lacona-phrase'
+import { URL } from '..'
+import { expect } from 'chai'
+import { Parser } from 'lacona'
 
-function from (i) {const a = []; for (let x of i) a.push(x); return a}
+function text(input) {
+  return _.map(input.words, 'text').join('')
+}
 
 describe('URL', () => {
   let parser, data
 
   beforeEach(() => {
     parser = new Parser()
+    parser.grammar = <URL />
   })
 
-  it('handles IP addresses with ports', () => {
-    parser.sentences = [<URL />]
-
-    data = from(parser.parse('http://127.0.0.1:57772'))
+  it('handles IP addresses', () => {
+    data = parser.parseArray('http://127.0.0.1')
     expect(data).to.have.length(1)
-    expect(fulltext.all(data[0])).to.equal('http://127.0.0.1:57772')
+    expect(text(data[0])).to.equal('http://127.0.0.1')
+    expect(data[0].result).to.equal('http://127.0.0.1')
+  })
+
+  it('handles normal URLs', () => {
+    data = parser.parseArray('http://google.com')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://google.com')
+    expect(data[0].result).to.equal('http://google.com')
+
+    data = parser.parseArray('http://google.com/mail')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://google.com/mail')
+    expect(data[0].result).to.equal('http://google.com/mail')
+  })
+
+  it('handles ports', () => {
+    data = parser.parseArray('http://127.0.0.1:57772')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://127.0.0.1:57772')
     expect(data[0].result).to.equal('http://127.0.0.1:57772')
 
-    data = from(parser.parse('http://127.0.0.300:57772'))
-    expect(data).to.have.length(0)
+    data = parser.parseArray('http://google.com:57772')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://google.com:57772')
+    expect(data[0].result).to.equal('http://google.com:57772')
+
+    data = parser.parseArray('http://google.com:57772/mail')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://google.com:57772/mail')
+    expect(data[0].result).to.equal('http://google.com:57772/mail')
+  })
+
+  it('adds http:// by default for basic urls', () => {
+    data = parser.parseArray('google.com')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://google.com')
+    expect(data[0].result).to.equal('http://google.com')
+
+    data = parser.parseArray('google.com/mail')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('http://google.com/mail')
+    expect(data[0].result).to.equal('http://google.com/mail')
+  })
+
+  it('suppresses incomplete URLs', () => {
+    data = parser.parseArray('google.c')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('URL')
+    expect(data[0].words[0].placeholder).to.be.true
+
+    data = parser.parseArray('goo')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('URL')
+    expect(data[0].words[0].placeholder).to.be.true
+
+    data = parser.parseArray('http:')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('URL')
+    expect(data[0].words[0].placeholder).to.be.true
+
+    data = parser.parseArray('https')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('URL')
+    expect(data[0].words[0].placeholder).to.be.true
   })
 })
